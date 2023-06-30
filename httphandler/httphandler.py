a67235a62526a0aecde70e437e8412908282b72e
poshl9k <poshl9k@gmail.com>
2023-06-30 14:23:30 +0300

    Добавлен автовыбор протокола

--- httphandler/httphandler.py
+++ httphandler/httphandler.py
@@ -5,32 +5,26 @@ import logging
 import os
 import socket
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename="log.txt",
-    encoding= 'utf-8',
-    style = '{',
-    format="{asctime},{levelname},{message}",
-)
 
-def log(text):
-    return logging.info(f"{socket.gethostname()},{text}")
-
+from c_logger import CLogger
 
 
 class HttpHandler:
     def __init__(self, address, login, password) -> None:
+        self.logger = CLogger()
         self.login = login
         self.password = password
         self.address = address
+        self.protocol = ""
         self.sn = self.create_session()
-        log("Сессия создана")
+        self.logger.log("Сессия создана")
 
     def create_session(self):
         sess = Session()
         sess.auth = (self.login, self.password)
         sess.verify = False
         sess.mount("http://", HTTPAdapter(max_retries=2))
+        sess.mount("https://", HTTPAdapter(max_retries=2))
         sess.headers.update(
             {
                 "accept": "application/json",
@@ -41,12 +35,37 @@ class HttpHandler:
 
     # написать метод для первостепенной проверки конекта
     def get_fw_rules(self):
-        return self.sn.get(
-            f"http://{self.address}/api/v1/firewall/nat/port_forward", timeout=1
+        if self.protocol == "":
+            self.set_protocol()
+        response = self.sn.get(
+            f"{self.protocol}{self.address}/api/v1/firewall/nat/port_forward",
+            timeout=1,
         )
+        return response
+
+    def set_protocol(self):
+        protocols = ["https://", "http://"]
+        try:
+            response = self.sn.get(
+                f"{protocols[0]}{self.address}/api/v1/firewall/nat/port_forward",
+                timeout=1,
+            )
+            self.protocol = protocols[0]
+            return response
+        except Exception as error:
+            pass
+        try:
+            response = self.sn.get(
+                f"{protocols[1]}{self.address}/api/v1/firewall/nat/port_forward",
+                timeout=1,
+            )
+            self.protocol = protocols[1]
+            return response
+        except:
+            pass
 
     def set_fw_rules(self, rules, disable=None):
-        response = []
+        responses = []
         for rule in rules:
             data = {
                 "id": rule["id"],
@@ -56,13 +75,17 @@ class HttpHandler:
                 data["disabled"] = False
             else:
                 data["disabled"] = True
-            response.append(
-                self.sn.put(
-                    f"http://{self.address}/api/v1/firewall/nat/port_forward",
+            try:
+                response = self.sn.put(
+                    f"{self.protocol}{self.address}/api/v1/firewall/nat/port_forward",
                     json=data,
                     timeout=2,
                 )
-            )
-        return response
+            except Exception as error:
+                pass
+            responses.append(response)
+        self.logger.log(responses)
+        return responses
+
 
 # с = HttpHandler('1','2','3')
